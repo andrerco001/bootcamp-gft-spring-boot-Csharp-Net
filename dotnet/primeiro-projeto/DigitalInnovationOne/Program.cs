c839416dcd43531697933687551273e81bd55655
andrerco001 <andreoliveira.adv@hotmail.com>
2020-04-21 14:09:01 -0300

    Continuacao do curso Csharp dotNet

---
+++ dotnet/primeiro-projeto/DigitalInnovationOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int numeroDeVezes = 5;
+
+            for(int i = 1; i <= numeroDeVezes; i++){
+                Console.WriteLine("Bem-vindo ao curso .Net " + i);
+            }
+
+
+            
+        }
+    }
+}
